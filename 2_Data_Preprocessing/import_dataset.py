5fb1c5e2d98a0b684fdbeecce8582f6488456ec5
NishkarshRaj <500060720@stu.upes.ac.in>
2020-01-16 19:10:15 +0530

    Day 6: Splitting Data Set in R

--- 2_Data_Preprocessing/import_dataset.py
+++ 2_Data_Preprocessing/import_dataset.py
@@ -10,14 +10,3 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
---
+++ 2_Data_Preprocessing/splitting_dataset.R
@@ -0,0 +1,12 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
