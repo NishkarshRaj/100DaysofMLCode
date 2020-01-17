6a835dcb2b0a02dbae7030135cbef0b852ce20c5
NishkarshRaj <500060720@stu.upes.ac.in>
2020-01-17 08:38:24 +0530

    Update Feature Scaling in Python

--- 2_Data_Preprocessing/feature_scaling.py
+++ 2_Data_Preprocessing/feature_scaling.py
@@ -15,9 +15,9 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
