a65ae4e0d88539e6cfdbd3ede2c41b2ca97ac9e4
NishkarshRaj <500060720@stu.upes.ac.in>
2020-01-23 13:12:55 +0530

    Day 14 | Multi Linear Regression in R updated

--- 3_Regression/Multi_Linear_Regression/R/multiple_linear_regression.R
+++ 3_Regression/Multi_Linear_Regression/R/multiple_linear_regression.R
@@ -26,3 +26,21 @@ regressor = lm(formula = Profit ~ .,
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+# Optional Step: Remove State2 only (as opposed to removing State directly)
+# regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + factor(State, exclude = 2),
+#                data = dataset)
+# summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
