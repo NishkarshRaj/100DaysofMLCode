d4837a585e58e9177a281e2e3d63d99d25430f07
Uday Kiran <kiranu941@gmail.com>
2020-10-02 06:49:02 +0530

    Modified Some Of the previous Files And added New Files in Deep_Learning Folder
    In the Previous file there were unwanted recommended installation for the particular file which were not being used in it. Coming to to the ANN folder inside the deep learning i have uploaded Hand written digit recognitation Project , and in CNN folder i have uploaded Cats Vs Dogs Project and many materials to learn Deep Learning. If In case there are any changes to it , let me know in mail "kiranu941@gmail.com"

--- 9_Deep_Learning/Artificial_Neural_Networks/ann.py
+++ 9_Deep_Learning/Artificial_Neural_Networks/ann.py
@@ -1,10 +1,5 @@
 # Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
 
 # Installing Keras
 # pip install --upgrade keras
