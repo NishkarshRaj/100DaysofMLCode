105aa32164b81f14459afa2b63c285b4cf577d07
codingcosmonaut <71237908+codingcosmonaut@users.noreply.github.com>
2020-10-03 05:19:12 +0200

    Fix broken imports in all chapters (#50)
    * fix broken imports; add main function
    
    * fix imports in chapter 2
    
    * fix imports in chapter 3
    
    * fix imports in chapter 5
    
    * fix imports in chapter 7
    
    * fix imports in chapter 8
    
    * fix imports in chapter 9
    
    * fix imports in chapter 10
    
    * fix imports in chapter 11; add xgboost to requirements list
    
    Co-authored-by: Nishkarsh Raj <500060720@stu.upes.ac.in>

--- 2_Data_Preprocessing/python/missing_data.py
+++ 2_Data_Preprocessing/python/missing_data.py
@@ -2,16 +2,22 @@
 """
 
 # Importing the libraries
-import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 
-# Importing the dataset
-dataset = pd.read_csv('../Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
 
-# Taking care of missing data
-imputer = SimpleImputer(missing_values=np.NaN, strategy='mean')
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+def main():
+    # Importing the dataset
+    dataset = pd.read_csv('../Data.csv')
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 3].values
+
+    # Taking care of missing data
+    imputer = SimpleImputer(missing_values='NaN', strategy='mean')
+    imputer = imputer.fit(X[:, 1:3])
+    X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+if __name__ == '__main__':
+    main()
+
--- 4_Classification/Classification_Comparison/classification_Comparison.py
+++ 4_Classification/Classification_Comparison/classification_Comparison.py
@@ -1,75 +1,83 @@
 # Classification  Comparison
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.preprocessing import LabelEncoder
-
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-
-#Convert gender into numeric
-labelEncoder_gender =  LabelEncoder()
-dataset.iloc[:,1] = labelEncoder_gender.fit_transform(dataset.iloc[:,1])
-
-# creating dependent and independent variables
-X = dataset.iloc[:, 1:4].values
-y = dataset.iloc[:, 4].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-#### Fitting different classifiers to the Training set ###
-###########################################################
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
-classifiers={
-"Decision Tree" : DecisionTreeClassifier(criterion = 'entropy', random_state = 0),
-"K Nearest Neighbors" : KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2),
-"Kernel SVM" : SVC(kernel = 'rbf', random_state = 0),
-"Logistic Regression" : LogisticRegression(random_state = 0, solver='lbfgs'),
-"Naive Bayes" : GaussianNB(),
-"Random Forest" : RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0),
-"Support Vector Machine": SVC(kernel = 'linear', random_state = 0)
-}
 
-adict={}
-# Fitting classifiers to the Training set
-for key in classifiers:
-    classifiers[key].fit(X_train, y_train)
-    # Predicting the Test set results
-    y_pred = classifiers[key].predict(X_test)    
-    # Making the Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
-    TP=cm[1,1]
-    TN=cm[0,0]
-    FP=cm[0,1]
-    FN=cm[1,0]
-    accuracy=accuracy_score(y_test, y_pred)
-    error=1-accuracy
-    Sensitivity=recall_score(y_test, y_pred)
-    Specificity=TN / (TN + FP)
-    False_Positive_Rate = 1-Specificity
-    Precision=precision_score(y_test, y_pred)
-    adict[key]=[accuracy, round(Precision,3), round(Sensitivity,3), round(Specificity,3) ,round(error,4), round(False_Positive_Rate,3)]
+def main():
+    # Importing the dataset
+    dataset = pd.read_csv('Social_Network_Ads.csv')
 
-print(pd.DataFrame.from_dict(adict, orient='index', columns=['Accuracy', 'Precision', 'Sensitivity', 'Specificity','Error', 'False_Positive_Rate']))
+    # Convert gender into numeric
+    labelEncoder_gender = LabelEncoder()
+    dataset.iloc[:, 1] = labelEncoder_gender.fit_transform(dataset.iloc[:, 1])
+
+    # creating dependent and independent variables
+    X = dataset.iloc[:, 1:4].values
+    y = dataset.iloc[:, 4].values
+
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    #### Fitting different classifiers to the Training set ###
+    ###########################################################
+
+    classifiers = {
+        "Decision Tree": DecisionTreeClassifier(criterion='entropy', random_state=0),
+        "K Nearest Neighbors": KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2),
+        "Kernel SVM": SVC(kernel='rbf', random_state=0),
+        "Logistic Regression": LogisticRegression(random_state=0, solver='lbfgs'),
+        "Naive Bayes": GaussianNB(),
+        "Random Forest": RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0),
+        "Support Vector Machine": SVC(kernel='linear', random_state=0)
+    }
+
+    adict = {}
+
+    # Fitting classifiers to the Training set
+    for key in classifiers:
+        classifiers[key].fit(X_train, y_train)
+        # Predicting the Test set results
+        y_pred = classifiers[key].predict(X_test)
+        # Making the Confusion Matrix
+        cm = confusion_matrix(y_test, y_pred)
+        TP = cm[1, 1]
+        TN = cm[0, 0]
+        FP = cm[0, 1]
+        FN = cm[1, 0]
+        accuracy = accuracy_score(y_test, y_pred)
+        error = 1 - accuracy
+        Sensitivity = recall_score(y_test, y_pred)
+        Specificity = TN / (TN + FP)
+        False_Positive_Rate = 1 - Specificity
+        Precision = precision_score(y_test, y_pred)
+        adict[key] = [accuracy, round(Precision, 3), round(Sensitivity, 3), round(Specificity, 3), round(error, 4),
+                      round(False_Positive_Rate, 3)]
+
+    print(pd.DataFrame.from_dict(adict, orient='index',
+                                 columns=['Accuracy', 'Precision', 'Sensitivity', 'Specificity', 'Error',
+                                          'False_Positive_Rate']))
+
+
+if __name__ == '__main__':
+    main()
